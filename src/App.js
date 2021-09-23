4d573de21438972c7d7b1d35f26c25dd366b8614
Nerdyxils <silasabiodun25@gmail.com>
2021-09-23 10:39:40 +0100

    collection input comp added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Switch, Route } from 'react-router';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 const HatsPage = () => (
@@ -13,7 +14,7 @@ function App() {
     <div>
       <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
       </Switch>
       
     </div>
